6e77c930b8c8fde8a5f428d9514a818af8aac517
Kabir Sidhu <kxbir414@gmail.com>
2025-04-25 22:16:30 -0700

    init commit

---
+++ src/main/java/com/website/roma_tarot_master/RomaTarotMasterApplication.java
@@ -0,0 +1,13 @@
+package com.website.roma_tarot_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomaTarotMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomaTarotMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/website/roma_tarot_master/RomaTarotMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.roma_tarot_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomaTarotMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
